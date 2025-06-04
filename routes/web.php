2dec2eca1261e932354caa47eea3ca8a6baa3307
Fraanj <franciscobalboutn@gmail.com>
2025-06-03 21:10:08 -0300

    implementacion validacion de rol en routes para gestion de reservas y maquinas

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ReservaController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MaquinariaController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
@@ -22,15 +23,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/reservas/pago', [ReservaController::class, 'pago'])->name('reservas.pago');
     Route::post('/reservas/pago', [ReservaController::class, 'pago'])->name('reservas.confirmarPago');
 });
-// Rutas para empleados y administradores
-Route::middleware(['auth', 'role:employee,admin'])->group(function () {
-    // Route::resource('maquinarias', MaquinariaController::class);
+// Rutas para empleados
+Route::middleware(['auth', 'role:employee'])->group(function () {
+    // Rutas para empleados
+});
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/maquinarias/CrearMaquina', [MaquinariaController::class, 'create'])->name('maquinarias.create');
+    Route::post('/maquinarias', [MaquinariaController::class, 'store']);
+    Route::get('/maquinarias/{id}', [MaquinariaController::class, 'show'])->name('maquinarias.show');
 });
 
-use App\Http\Controllers\MaquinariaController;
-
-Route::get('/maquinarias/CrearMaquina', [MaquinariaController::class, 'create'])->name('maquinarias.create');
-Route::post('/maquinarias', [MaquinariaController::class, 'store']);
-Route::get('/maquinarias/{id}', [MaquinariaController::class, 'show'])->name('maquinarias.show');
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
