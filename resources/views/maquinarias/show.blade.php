df0cd5daa844ad994e2fcdeccadaa986758ea728
Fraanj <franciscobalboutn@gmail.com>
2025-06-13 11:26:59 -0300

    debug click maquina disponible sin estar autenticado rompia todo

--- resources/views/maquinarias/show.blade.php
+++ resources/views/maquinarias/show.blade.php
@@ -262,7 +262,10 @@
                     <input type="text" name="fecha_fin" placeholder="Fecha fin" autocomplete="off">
                 </div>
                 <button type="submit" class="btn-alquilar"
-                        @if($maquinaria->disponibilidad_id != 1 || (Auth::user()->isAdmin())) disabled @endif
+                        @if($maquinaria->disponibilidad_id != 1) disabled @endif
+                        @auth
+                            @if(    (Auth::user()->isAdmin()) || (Auth::user()->isEmployee())  )  disabled  @endif
+                        @endauth
                         onmouseover="if(!this.disabled) this.style.backgroundColor='#d6640d'"
                         onmouseout="if(!this.disabled) this.style.backgroundColor='#f97316'">
                     Reservar
