4722ba8173f102b474150c61546340c4b27fc473
Fraanj <franciscobalboutn@gmail.com>
2025-06-13 11:28:07 -0300

    agrego comentarios

--- resources/views/maquinarias/show.blade.php
+++ resources/views/maquinarias/show.blade.php
@@ -262,8 +262,8 @@
                     <input type="text" name="fecha_fin" placeholder="Fecha fin" autocomplete="off">
                 </div>
                 <button type="submit" class="btn-alquilar"
-                        @if($maquinaria->disponibilidad_id != 1) disabled @endif
-                        @auth
+                        @if($maquinaria->disponibilidad_id != 1) disabled @endif  // si la maquina no esta disponible no se puede reservar
+                        @auth // si el usuario es admin o empleado no se puede reservar
                             @if(    (Auth::user()->isAdmin()) || (Auth::user()->isEmployee())  )  disabled  @endif
                         @endauth
                         onmouseover="if(!this.disabled) this.style.backgroundColor='#d6640d'"
